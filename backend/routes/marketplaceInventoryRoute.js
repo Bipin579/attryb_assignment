2e4805102ee07e72b760b109a31bcf6666f52f41
Bipin <biping579@gmail.com>
2023-06-07 18:00:10 +0530

    bug fixed

--- backend/routes/marketplaceInventoryRoute.js
+++ backend/routes/marketplaceInventoryRoute.js
@@ -1,9 +1,8 @@
 const express = require("express");
 const router = express.Router();
-const isAuthenticated = require("../middleware/authenticate");
 const { getAllProducts} = require("../controller/marketplaceInventoryController");
 
 router.get("/", getAllProducts);
 
 
-module.exports = router;
+module.exports = router
